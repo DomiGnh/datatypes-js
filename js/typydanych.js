3b9db452f5aa8cefc3a205dffcd78f00c8361f06
Dominika Gnich <d.gnich@wp.pl>
2017-06-10 10:49:41 +0200

    boolean

--- js/typydanych.js
+++ js/typydanych.js
@@ -15,10 +15,59 @@ var wyplataStr= 5000;
 var premiaStr= "2500";
 var calkowitaWyplataStr;
 
-calkowitaWyplataStr= wyplataStr + premiaStr;
+calkowitaWyplataStr= wyplataStr + ' "dodajemy" '+ premiaStr;
 
 console.log(calkowitaWyplataStr);
 
 /*przy dodawaniu typów gdy jeden elemnt bedzie stringiem, to js zamieni wszytskie inne na stringi*/
 
 /*przy dodawaniu typy liczbowe się zsumują, a typy string wyświetlą obok siebie bez spacji,doda nie liczbe a ciąg znaków*/
+
+
+//metody dla zmiennych string//
+
+var imieStudenta = " Michał";
+console.log(imieStudenta.indexOf('c'));
+console.log(imieStudenta);
+console.log(imieStudenta.trim());
+
+//komputer zawsze liczy od 0!!!!//
+
+
+//typ logiczny- boolean//
+
+var czyJestSmog;
+czyJestSmog = false;
+
+if (czyJestSmog == true){
+    console.log("Jest Smog");
+}
+else {
+    console.log("nie ma smoga :)");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
